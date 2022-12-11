a694564f1e1dc416afba36d229659bc273e86ea9
Andrey <androrvit@gmail.com>
2022-12-11 19:52:40 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React, { useState } from "react";
+import { getForecast } from "./API/forecastApi";
+
 function App() {
-    return <div className="App"></div>;
+    const [symbol, setSymbol] = useState("");
+    const [data, setData] = useState({});
+
+    return (
+        <div className="App">
+            <input value={symbol} onChange={(e) => setSymbol(e.target.value)} />
+            <button onClick={async () => setData(await getForecast(symbol))}>
+                Построить график
+            </button>
+            <div>{JSON.stringify(data.historicData)}</div>
+        </div>
+    );
 }
 
 export default App;
