4adb2bdf8d6a777438913c7a0b3a1da815ec89d9
Andrey <androrvit@gmail.com>
2022-12-11 19:55:27 +0300

    fix getForecast

--- src/API/forecastApi.js
+++ src/API/forecastApi.js
@@ -1,6 +1,6 @@
 import { host } from ".";
 
 export const getForecast = async (symbol) => {
-    const { data } = await host.get(`api/stock/{symbol}`);
+    const { data } = await host.get(`api/stock/${symbol}`);
     return data;
 };
