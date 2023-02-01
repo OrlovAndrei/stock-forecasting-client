b858af6fafe3c589eb73480acbdec5cf20311fa5
Andrey <androrvit@gmail.com>
2023-02-01 12:26:36 +0300

    change api

--- src/API/forecastApi.js
+++ src/API/forecastApi.js
@@ -1,6 +1,13 @@
 import { host } from ".";
 
-export const getForecast = async (symbol) => {
-    const { data } = await host.get(`api/stock/${symbol}`);
+export const getForecast = async (symbol, method = "ssa") => {
+    const { data } = await host.get(
+        `api/stock/getforecast?symbol=${symbol}&method=${method}`
+    );
+    return data;
+};
+
+export const getMethods = async () => {
+    const { data } = await host.get(`api/stock/getmethods`);
     return data;
 };
